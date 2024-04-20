f39c71573cb1db600fed66f4d5452cb8cac67581
Manet <hoeunmaneth09@gmail.com>
2024-04-20 22:31:27 +0700

    done

--- core/home/templatetags/custom_filters(1).py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def starts_with(value, arg):
-    """Check if a string starts with a specific prefix."""
-    return value.startswith(arg)
